d275c79b9654128e240c61027087979c47aeb3b1
lesa <mwabalesa1497@gmail.com>
2025-03-31 23:43:54 +0000

    first realese using git!

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hola</title>
+</head>
+<body>
+    
+<php?
+    echo "c'est bien";
+    echo "le git";
+?>
+</body>
+</html>
+
